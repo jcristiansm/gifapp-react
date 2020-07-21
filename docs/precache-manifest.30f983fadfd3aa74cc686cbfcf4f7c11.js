56a8dad25372e9e3ae1c0ed8eca3210e428ce4c7
babygroot <cristiansm1795@gmail.com>
2020-07-21 00:44:14 -0500

    renombrar carptea build->docs

---
+++ docs/precache-manifest.30f983fadfd3aa74cc686cbfcf4f7c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6579069cf8e187290666aa19e149e79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "261ba828039c85411642",
+    "url": "/static/css/main.09db7a00.chunk.css"
+  },
+  {
+    "revision": "1666b5b5f6a3a0c719b9",
+    "url": "/static/js/2.984f57f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.984f57f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "261ba828039c85411642",
+    "url": "/static/js/main.392cd5ac.chunk.js"
+  },
+  {
+    "revision": "903e7817fc2a18360c09",
+    "url": "/static/js/runtime-main.5142bbd6.js"
+  }
+]);
